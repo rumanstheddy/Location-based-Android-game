aa5445d54bf5d739143933de80bcb817cc9f18d7
Sum-RED <Sum-RED@.noreply.github.com>
2019-12-08 21:44:37 -0800

    Added Scenes and Scripts

---
+++ Scripts/PinchToZoom.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class PinchToZoom : MonoBehaviour
+{
+    public float perspectiveZoomSpeed = 0.5f;        // The rate of change of the field of view in perspective mode.
+    public float orthoZoomSpeed = 0.5f;        // The rate of change of the orthographic size in orthographic mode.
+    public Camera cam;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        // If there are two touches on the device...
+        if (Input.touchCount == 2)
+        {
+            // Store both touches.
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            // Find the position in the previous frame of each touch.
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            // Find the magnitude of the vector (the distance) between the touches in each frame.
+            float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+            // Find the difference in the distances between each frame.
+            float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+
+            // If the camera is orthographic...
+            if (cam.orthographic)
+            {
+                // ... change the orthographic size based on the change in distance between the touches.
+                cam.orthographicSize += deltaMagnitudeDiff * orthoZoomSpeed;
+
+                // Make sure the orthographic size never drops below zero.
+                cam.orthographicSize = Mathf.Max(cam.orthographicSize, 0.1f);
+            }
+            else
+            {
+                // Otherwise change the field of view based on the change in distance between the touches.
+                cam.fieldOfView += deltaMagnitudeDiff * perspectiveZoomSpeed;
+
+                // Clamp the field of view to make sure it's between 0 and 180.
+                cam.fieldOfView = Mathf.Clamp(cam.fieldOfView, 49.9f, 89.9f);
+            }
+        }
+    }
+}
---
+++ Scripts/UIManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    //[SerializeField] private Text levelText;
+    [SerializeField] private GameObject menu;
+    [SerializeField] private GameObject menuButton;
+    [SerializeField] private GameObject playButton;
+
+    public string MyWorld { get; private set; }
+
+    private void Awake()
+    {
+        //Assert.IsNotNull(levelText);
+        Assert.IsNotNull(menu);
+    }
+
+    public void toggleMenu()
+    {
+        menu.SetActive(!menu.activeSelf);
+    }
+
+    //Set the button available on the device's screen
+    public void menubuttonClicked()
+    {
+        menuButton.SetActive(false);
+    }
+
+    public void TogglePause()
+    {
+        //Time.timeScale is to slow down the rate at which the frames are rendered
+        Time.timeScale = Mathf.Approximately(Time.timeScale, 0.0f) ? 1.0f : 0.0f;
+    }
+
+    public void playbuttonClicked()
+    {
+        //Time.timescale value is set to 1, once the play button is clicked
+        Time.timeScale = 1;
+        menu.SetActive(false);
+        menuButton.SetActive(true);
+    }
+}
